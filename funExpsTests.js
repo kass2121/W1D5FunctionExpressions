d91c796260e6476e337342dc453687bdc396c505
Kassaw  Ayalew <79111500+kass2121@users.noreply.github.com>
2021-09-06 10:54:20 -0400

    Update funExpsTests.js

--- funExpsTests.js
+++ funExpsTests.js
@@ -8,7 +8,7 @@ const imports = require("./fnExps.js");  //with node need the name of your file 
 const double = imports.double;  //do this for all of the functions used in the Mocha tests
 const myMap = imports.myMap;
 const times100 = imports.times100;
-
+const triples = imports.triples;
 
 /* the following comment is needed when you run in the browser environment */
 /* global assert double times100 myMap  */
